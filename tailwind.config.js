87ea751e7cc07ba6c78a480305a9a410ca60bd32
Nant1ng <andressantana99@hotmail.se>
2021-04-20 20:46:28 +0200

    Made so I can use more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,3 +10,21 @@ module.exports = {
   },
   plugins: [],
 }
+
+// So you can get access to all the colors
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+      teal: colors.teal,
+    }
+  }
+}
