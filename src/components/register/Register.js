cc787ea690e8d41cc4f1d3e61d38fe0d5831c672
Nant1ng <andressantana99@hotmail.se>
2021-04-22 21:59:50 +0200

    Register from half done

---
+++ src/components/register/Register.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+function Register() {
+    return (
+        <div className="font-sans">
+      <div className="relative min-h-screen flex flex-col sm:justify-center items-center">
+        <div className="relative sm:max-w-sm w-full">
+          <div className="card bg-teal-300 shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
+          <div className="card bg-gray-500 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
+          <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
+            <label className="block mt-3 text-xl text-gray-700 text-center font-semibold">
+              Sign Up
+            </label>
+            <form method="#" action="#" className="mt-10">
+              <div>
+                <input
+                  type="username"
+                  placeholder=" Username"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                />
+              </div>
+
+              <div className="mt-3">
+                <input
+                  type="email"
+                  placeholder=" Email"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                />
+              </div>
+
+              <div className="mt-7">
+                <input
+                  type="password"
+                  placeholder=" Password"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                />
+              </div>
+
+              <div className="1">
+                <input
+                  type="password"
+                  placeholder=" Confirm Password"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                />
+              </div>
+
+              <div className="mt-7 flex">
+                <label
+                  htmlFor="remember_me"
+                  className="inline-flex items-center w-full cursor-pointer"
+                >
+                </label>
+              </div>
+
+              <div className="mt-7">
+                <button className="text-gray-900 bg-teal-300 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105">
+                  Create Account
+                </button>
+              </div>
+
+              
+
+
+                
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Register
