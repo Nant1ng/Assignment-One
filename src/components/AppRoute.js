3a3e335ffac3cb657422afcbe25dba06bdf6ed80
Nant1ng <andressantana99@hotmail.se>
2021-04-23 22:59:57 +0200

    Commit

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./header/Header";
-import Footer from "./footer/Footer"
+import HomePage from "./homePage/HomePage";
+import Footer from "./footer/Footer";
 import Login from "./login/LogIn";
 import Register from "./register/Register";
 
 import Buy from "./buy/Buy";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+
 
 export default function AppRoute() {
   return (
@@ -15,8 +17,9 @@ export default function AppRoute() {
 
         <Route path="/Log-in" exact component={Login} />
         <Route path="/Register" exact component={Register}></Route>
-
         <Route path="/Buy" exact component={Buy}></Route>
+
+        <Route path="/" exact component={HomePage}></Route>
         <Footer />
       </Router>
     </>
