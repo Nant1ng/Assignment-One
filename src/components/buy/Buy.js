87b9f55dccde8455d4fa1e873c3dc623a48c97d1
Nant1ng <andressantana99@hotmail.se>
2021-04-23 11:27:24 +0200

    Buy

---
+++ src/components/buy/Buy.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardList from "../card/CardList";
+
+function Buy() {
+    return (
+        <div>
+            <CardList />
+        </div>
+    )
+}
+
+export default Buy
