65a1855cec78d1d6eff64e029ae185c570476f25
Nant1ng <andressantana99@hotmail.se>
2021-04-24 17:33:17 +0200

    HomePage

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -3,9 +3,9 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div className="box-content h-screen">
+    
       <div class="box-border h-32 w-32 p-4 border-4 "></div>
-    </div>
+    
   );
 }
 
