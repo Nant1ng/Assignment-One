b16d302cfe63829b99855151935afbadb7c42f33
Nant1ng <andressantana99@hotmail.se>
2021-04-30 19:43:38 +0200

    Removed dummy text

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -4,10 +4,7 @@ function HomePage() {
   return (
     <div className="flex justify-center mt-56">
       <div className="box-border h-96 w-96 p-4 border-4 ">
-        Kommer försöka få in en map plug-in här senare. 
-        Men alla links och arrayer för produkter funkar, 
-        förutom bild men den är i kodad istället.
-        Kommer ändra på det senare.     
+          
       </div>
     </div>
   );
