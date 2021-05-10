94f7f535220d54954b9c2401d9ffe68d5fa38a1d
Nant1ng <andressantana99@hotmail.se>
2021-05-10 15:54:41 +0200

    Log out

--- src/components/logOut/LogOut.js
+++ src/components/logOut/LogOut.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function LogOut() {
+  const history = useHistory();
+
   function handleClick(e) {
     e.preventDefault();
     localStorage.removeItem("Token");
+    history.push("/");
     window.location.reload();
   }
 
   return (
-    <Link to="/">
-      <button
-        onClick={handleClick}
-        className="bg-teal-400 hover:bg-teal-400 text-gray-500 font-bold py-2 px-4 border-b-4 border-teal-600 hover:border-teal-500 rounded"
-      >
-        LOG OUT plz
-      </button>
-    </Link>
+    <button
+      onClick={handleClick}
+      className="bg-teal-400 hover:bg-teal-400 text-gray-500 font-bold py-2 px-4 border-b-4 border-teal-600 hover:border-teal-500 rounded"
+    >
+      LOG OUT plz
+    </button>
   );
 }
 
