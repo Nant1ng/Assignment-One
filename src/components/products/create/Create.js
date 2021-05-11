856e9b991c7951461cbbca767d6c3c71f14c45b0
Nant1ng <andressantana99@hotmail.se>
2021-05-11 15:01:53 +0200

    Create products

---
+++ src/components/products/create/Create.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Create() {
+  const [EnteredTitle, setTitle] = useState("");
+  const [EnteredDescription, setDescription] = useState("");
+  const [EnteredPrice, setPrice] = useState("");
+  const [EnteredImage, setImage] = useState("");
+
+  function titleChangeHandler(event) {
+    setTitle(event.target.value);
+  }
+
+  function descriptionChangeHandler(event) {
+    setDescription(event.target.value);
+  }
+
+  function priceChangeHandler(event) {
+    setPrice(event.target.value);
+  }
+
+  function imageChangeHandler(event) {
+    setImage(event.target.files[0]);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:1337/products", {
+        title: EnteredTitle,
+        description: EnteredDescription,
+        price: EnteredPrice,
+      })
+      .then((response) => {
+        console.log(response.data);
+
+        const data = new FormData();
+
+        data.append("files", EnteredImage);
+        data.append("ref", "product");
+        data.append("refId", response.data.id);
+        data.append("field", "img");
+
+        axios
+          .post("http://localhost:1337/upload", data)
+          .then((image) => console.log(image))
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          value={EnteredTitle}
+          onChange={titleChangeHandler}
+          placeholder="Title"
+        />
+        <label>Description</label>
+        <input
+          type="text"
+          name="description"
+          value={EnteredDescription}
+          onChange={descriptionChangeHandler}
+          placeholder="Description"
+        />
+        <label>Price</label>
+        <input
+          type="number"
+          name="price"
+          value={EnteredPrice}
+          onChange={priceChangeHandler}
+          placeholder="Price"
+        />
+        <label>Image</label>
+        <input
+          type="file"
+          name="file"
+          onChange={imageChangeHandler}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Create;
