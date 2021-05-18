cd8da01e5d4628d95cefdd1e87580d9d6a225ad5
Nant1ng <andressantana99@hotmail.se>
2021-05-18 14:17:08 +0200

    Modal

--- src/components/products/buy/cardBuy/Card.js
+++ src/components/products/buy/cardBuy/Card.js
@@ -1,7 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import axios from "axios";
+import Modal from "react-modal";
 
 function Card({ title, price, desc, image }) {
+  Modal.setAppElement("#root");
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+  
   return (
     <>
       <div className="flex-auto flex-col flow-root">
@@ -22,12 +37,14 @@ function Card({ title, price, desc, image }) {
                 {price} $
               </p>
               <p className="mt-2 text-teal-300">{desc}</p>
-              <Link
+              {/*<Link */}
+              <button
                 className="bg-teal-300 justify-center text-center rounded-lg shadow px-10 py-2 flex items-center"
                 to="/Checkout"
               >
                 Buy
-              </Link>
+              </button>
+              {/*</Link> */}
             </div>
           </div>
         </div>
