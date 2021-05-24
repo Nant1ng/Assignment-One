d40f4ef0a23567903f0ec2ec94ea377764ca5d2e
Nant1ng <andressantana99@hotmail.se>
2021-05-24 14:42:48 +0200

    Commit

--- src/components/login/LogIn.js
+++ src/components/login/LogIn.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 
 function Login() {
