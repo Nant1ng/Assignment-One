0ac0ada2c3575dcba0204058e9ca4b2907338195
Nant1ng <andressantana99@hotmail.se>
2021-05-24 14:56:53 +0200

    Commit

--- src/components/formValidation/useForm.js
+++ src/components/formValidation/useForm.js
@@ -1,36 +1,37 @@
-import { useState, useEffect } from "react";
+// Får den inte funka exakt som jag vill, så får kolla på den senare...  
+//import { useState, useEffect,  } from "react";
 
-function useForm(callback, validate) {
-  const [values, setValues] = useState({ email: "", password: "" });
-  const [errors, setErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
+// function useForm(callback, validate) {
+//   const [values, setValues] = useState({ email: "", password: "" });
+//   const [errors, setErrors] = useState({});
+//   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  }
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+//     setValues({
+//       ...values,
+//       [name]: value,
+//     });
+//   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    setErrors(validate(values));
-    setIsSubmitting(true);
-  }
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     setErrors(validate(values));
+//     setIsSubmitting(true);
+//   }
 
-  useEffect(() => {
-    if (Object.keys(errors).length === 0 && isSubmitting) {
-      callback();
-    }
-  }, [errors]);
+//    useEffect(() => {
+//      if (Object.keys(errors).length === 0 && isSubmitting) {
+//        callback();
+//      }
+//    }, [errors]);
 
-  return {
-    handleChange,
-    handleSubmit,
-    values,
-    errors,
-  };
-}
+//    return {
+//      handleChange,
+//      handleSubmit,
+//      values,
+//      errors,
+//    };
+// }
 
-export default useForm;
+// export default useForm;
