1716044028c95c9129ce4fef7649f13faa312f8e
Nant1ng <andressantana99@hotmail.se>
2021-06-08 11:10:31 +0200

    Commit

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -15,8 +15,8 @@ import Create from "./products/create/Create";
 
 import Buy from "./products/buy/Buy";
 import Rent from "./products/rent/Rent";
-import MyPurchases from "./myPurchases/MyPurchases";
-// import PurchaseList from "./myPurchases/PurchaseList";
+//import MyPurchases from "./myPurchases/MyPurchases";
+import PurchaseList from "./myPurchases/PurchaseList";
 
 import ContactUs from "./contactUs/ContactUs";
 
@@ -38,7 +38,7 @@ export default function AppRoute() {
 
           <Route path="/Buy" component={Buy} />
           <Route path="/Rent" component={Rent} />
-          <Route path="/My-Purchases" component={MyPurchases} />
+          <Route path="/My-Purchases" component={PurchaseList} />
           {/* <Route path="/PurchaseList" component={PurchaseList} /> */}
           <Route path="/Contact-Us" component={ContactUs} />
         </div>
